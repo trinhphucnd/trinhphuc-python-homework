73c0bb48f8db570269ec43572b9e3acb5ee3efae
Trinh Phuc <trinhphuc.ox@gmail.com>
2019-04-24 23:52:00 +0700

    test

--- session02/mutiltab.py
+++ session02/mutiltab.py
@@ -3,7 +3,10 @@ n = int(input("Enter the number :"))
 for i in range(1,n+1,1):
   for j in range(1,n+1,1):
     m=i*j
-    print(m,'\t',end="")
-    
+    if m<10:
+      print(m,end="  ")
+    else:
+      print(m,end=" ")
+ 
    
   print()
